443292911e139a1df3b805047e81c206d4ab2a27
Yongxuan Wang <yongwa@microsoft.com>
2020-09-27 08:24:57 -0700

    init

---
+++ MyModule/src/ResolveMyCmdletCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace MyModule
+{
+    [Cmdlet(VerbsDiagnostic.Resolve, "MyCmdlet")]
+    public class ResolveMyCmdletCommand : PSCmdlet
+    {
+        [Parameter(Position = 0)]
+        public Object InputObject { get; set; }
+
+        protected override void EndProcessing()
+        {
+            this.WriteObject(this.InputObject);
+            base.EndProcessing();
+        }
+    }
+}
